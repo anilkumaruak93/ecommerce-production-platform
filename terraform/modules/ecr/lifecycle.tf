141c0818f9be0e53c0e3a36bc983af384df4e638
anilgitsample <anilkumaru489@gmail.com>
2026-07-09 21:12:48 +0530

    feat(terraform): add ECR lifecycle policies

---
+++ terraform/modules/ecr/lifecycle.tf
@@ -0,0 +1,25 @@
+resource "aws_ecr_lifecycle_policy" "this" {
+  for_each = aws_ecr_repository.this
+
+  repository = each.value.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+
+        description = "Keep only the latest 5 images"
+
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 5
+        }
+
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
